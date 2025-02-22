ae7842d01f49d9bff6dfbd5dd3cb7b4916442591
shinPark43 <141878853+shinPark43@users.noreply.github.com>
2025-02-22 16:15:37 -0600

    Created Login page

--- CampusMind/frontend/app/(tabs)/index.jsx
+++ CampusMind/frontend/app/(tabs)/index.jsx
@@ -2,10 +2,16 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import { Link } from 'expo-router'
+import Icon from 'react-native-vector-icons/Feather';
 
 const LoginPage = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  }
 
   const handleLogin = () => {
     if (username === '' || password === '') {
@@ -22,24 +28,38 @@ const LoginPage = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Welcome to CampusMind</Text>
-      <Text style={styles.sub_header}>Sign into your account</Text>
+      <View style={styles.iconContainer}>
+        <Icon name="message-square" size={70} color='#fff'/>
+      </View>
+      <Text style={styles.header}>CampusMind</Text>
+      <Text style={styles.sub_header}>Welcome Back.</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
+      <View style={styles.inputContainer}>
+        <Icon name="mail" size={20} color="#888" style={styles.icon}/>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          placeholderTextColor="#888"
+          value={username}
+          onChangeText={setUsername}
+        />
+      </View>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      
-      />
+      <View style={styles.inputContainer}>
+        <Icon name="lock" size={20} color="#888" style={styles.icon}/>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="#888"
+          secureTextEntry={!passwordVisible}
+          value={password}
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity onPress={togglePasswordVisibility} style={styles.eyeIcon}>
+          <Icon name={passwordVisible ? 'eye-off' : 'eye'} size={20} color='#888' />
+        </TouchableOpacity>
+      </View>
+
       <View style={styles.forgot_password_container}>
         <TouchableOpacity onPress={handleForgotPassword}>
           <Text style={styles.forgot_password}>Forgot Password?</Text>
@@ -49,6 +69,10 @@ const LoginPage = () => {
       <Link href="/explore" style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Login</Text>
       </Link>
+
+      <TouchableOpacity style={styles.button_signUp}>
+        <Text style={styles.buttonText} numberOfLines={1}>Sign Up</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -62,9 +86,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#1B263B',
   },
   header: {
-    fontSize: 27,
+    fontSize: 50,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 15,
     color: '#FFF',
   },
   sub_header: {
@@ -72,17 +96,32 @@ const styles = StyleSheet.create({
     marginBottom: 40,
     color: '#FFF',
   },
-  input: {
-    width: '100%',
-    padding: 10,
+  icon: {
+    marginRight: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    paddingVertical: 12,
+    paddingHorizontal: 15,
     marginBottom: 15,
-    borderBottomWidth: 2,
-    borderBottomColor: '#778DA9',
-    color: '#FFF',
-    borderRadius: 5,
+    // borderBottomColor: '#778DA9',
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    width: '100%',
+  },
+  eyeIcon: {
+    padding: 2,
+  },
+  input: {
+    flex: 1,
+    fontSize: 13,
+    color: '#333',
   },
   forgot_password: {
-    color: '#B0C4E0',
+    color: '#FFF',
     fontSize: 12,
   },
   forgot_password_container: {
@@ -90,16 +129,28 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
   },
   button: {
-    marginTop: 25,
+    marginTop: 10,
     backgroundColor: '#778DA9',
-    paddingVertical: 12,
-    paddingHorizontal: 140,
-    borderRadius: 10,
+    paddingVertical: 15,
+    width: '100%',
+    borderRadius: 15,
+  },
+  button_signUp: {
+    marginTop: 10,
+    backgroundColor: '#778FFF',
+    paddingVertical: 15,
+    width: '100%',
+    borderRadius: 15,
   },
   buttonText: {
     color: '#fff',
+    textAlign: 'center',
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  iconContainer: {
+    alignItems: 'center',
+    marginBottom: 50,
   },
 });
 
